1a8c9c0708c6c35fabc260a39ec8c1eb79bac04e
abdulbaseer102 <147842675+abdulbaseer102@users.noreply.github.com>
2024-03-24 08:29:55 +0500

    My PIAIC Projects

---
+++ Project11_bank/Project10/index.js
@@ -0,0 +1,27 @@
+//make a bot
+import inquirer from "inquirer";
+inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    }
+])
+    .then((answers) => {
+    console.log(answers);
+});
